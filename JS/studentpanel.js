1b3cc010dbdbbabffa061da7ba2377731b8f2b4c
Halley Harper <qwrty.streams@gmail.com>
2024-10-09 21:58:03 -0400

    some changes idfk
    ik i changed the title for the student panel but thats pretty much it

--- JS/studentpanel.js
+++ JS/studentpanel.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import {
-    getAuth,
-    onAuthStateChanged,
+  getAuth,
+  onAuthStateChanged,
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import {
-    getFirestore,
-    doc,
-    getDoc,
+  getFirestore,
+  doc,
+  getDoc,
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const app = initializeApp(window.firebaseConfig);
@@ -14,32 +14,32 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 
 onAuthStateChanged(auth, async (user) => {
-    if (user) {
-        const userDoc = doc(db, "users", user.uid);
-        const docSnap = await getDoc(userDoc);
+  if (user) {
+    const userDoc = doc(db, "users", user.uid);
+    const docSnap = await getDoc(userDoc);
 
-        if (!docSnap.exists()) {
-            window.location.href = "../";
-        } else {
-            const userData = docSnap.data();
-            if (userData.admin) {
-                console.log("User is an admin.");
-                window.location.href = "../adminpanel/";
-            } else {
-                console.log("User is not an admin.");
-                const pfp = document.getElementById("profile-picture");
-                const placeholder = document.getElementById("default-profile-picture");
-                if (userData.profilePicture) {
-                    pfp.src = userData.profilePicture;
-                    placeholder.style.display = "none";
-                } else {
-                    pfp.style.display = "none";
-                    placeholder.style.display = "block";
-                }
-            }
-        }
+    if (!docSnap.exists()) {
+      window.location.href = "../";
     } else {
-        console.log("No user signed in.");
-        window.location.href = "../";
+      const userData = docSnap.data();
+      if (userData.admin) {
+        console.log("User is an admin.");
+        window.location.href = "../adminpanel/";
+      } else {
+        console.log("User is not an admin.");
+        const pfp = document.getElementById("profile-picture");
+        const placeholder = document.getElementById("default-profile-picture");
+        if (userData.profilePicture) {
+          pfp.src = userData.profilePicture;
+          placeholder.style.display = "none";
+        } else {
+          pfp.style.display = "none";
+          placeholder.style.display = "block";
+        }
+      }
     }
+  } else {
+    console.log("No user signed in.");
+    window.location.href = "../";
+  }
 });
