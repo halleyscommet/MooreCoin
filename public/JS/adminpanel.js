a8c6832dd849b28d5ee57865e2d0917e19579294
Halley Harper <qwrty.streams@gmail.com>
2024-10-14 15:54:48 -0400

    format js and json

--- public/JS/adminpanel.js
+++ public/JS/adminpanel.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import {
-    getAuth,
-    onAuthStateChanged,
+  getAuth,
+  onAuthStateChanged,
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import {
-    getFirestore,
-    doc,
-    getDoc,
+  getFirestore,
+  doc,
+  getDoc,
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const app = initializeApp(window.firebaseConfig);
@@ -14,23 +14,23 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 
 onAuthStateChanged(auth, async (user) => {
-    if (user) {
-        const userDoc = doc(db, "users", user.uid);
-        const docSnap = await getDoc(userDoc);
+  if (user) {
+    const userDoc = doc(db, "users", user.uid);
+    const docSnap = await getDoc(userDoc);
 
-        if (!docSnap.exists()) {
-            window.location.href = "../";
-        } else {
-            const userData = docSnap.data();
-            if (!userData.admin) {
-                console.log("User is not an admin.");
-                window.location.href = "../studentpanel/";
-            } else {
-                console.log("User is an admin.");
-            }
-        }
+    if (!docSnap.exists()) {
+      window.location.href = "../";
     } else {
-        console.log("No user signed in.");
-        window.location.href = "../";
+      const userData = docSnap.data();
+      if (!userData.admin) {
+        console.log("User is not an admin.");
+        window.location.href = "../studentpanel/";
+      } else {
+        console.log("User is an admin.");
+      }
     }
+  } else {
+    console.log("No user signed in.");
+    window.location.href = "../";
+  }
 });
--- public/JS/tablemanager.js
+++ public/JS/tablemanager.js
@@ -1,153 +1,161 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import {
-    getFirestore,
-    collection,
-    getDocs,
-    doc,
-    updateDoc,
-    onSnapshot,
+  getFirestore,
+  collection,
+  getDocs,
+  doc,
+  updateDoc,
+  onSnapshot,
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const app = initializeApp(window.firebaseConfig);
 const db = getFirestore(app);
 
 function setupRealTimeListener() {
-    const usersCol = collection(db, "users");
-    onSnapshot(usersCol, (snapshot) => {
-        const userList = snapshot.docs.map(doc => ({ uid: doc.id, ...doc.data() }));
-        populateTable(userList);
-    });
+  const usersCol = collection(db, "users");
+  onSnapshot(usersCol, (snapshot) => {
+    const userList = snapshot.docs.map((doc) => ({
+      uid: doc.id,
+      ...doc.data(),
+    }));
+    populateTable(userList);
+  });
 }
 
 function populateTable(users) {
-    const tableBody = document.getElementById("users-table-body");
-    tableBody.innerHTML = ""; // Clear existing table rows
+  const tableBody = document.getElementById("users-table-body");
+  tableBody.innerHTML = ""; // Clear existing table rows
 
-    users.forEach(user => {
-        if (user.admin) {
-            return;
-        }
+  users.forEach((user) => {
+    if (user.admin) {
+      return;
+    }
 
-        const row = document.createElement("tr");
+    const row = document.createElement("tr");
 
-        const nameCell = document.createElement("td");
-        nameCell.textContent = user.displayName;
-        row.appendChild(nameCell);
+    const nameCell = document.createElement("td");
+    nameCell.textContent = user.displayName;
+    row.appendChild(nameCell);
 
-        const emailCell = document.createElement("td");
-        emailCell.innerHTML = `<a href="mailto:${user.email}" target="_blank" id="email">${user.email}</a>`;
-        row.appendChild(emailCell);
+    const emailCell = document.createElement("td");
+    emailCell.innerHTML = `<a href="mailto:${user.email}" target="_blank" id="email">${user.email}</a>`;
+    row.appendChild(emailCell);
 
-        const hourCell = document.createElement("td");
-        hourCell.innerHTML = `  <form class="coin-edit">
+    const hourCell = document.createElement("td");
+    hourCell.innerHTML = `  <form class="coin-edit">
                                     <input type="number" class="coin-edit-input" value="${user.hour}" min="1" max="6">
                                     <button type="button" class="coin-edit-confirm">Confirm</button>
                                 </form>`;
-        row.appendChild(hourCell);
+    row.appendChild(hourCell);
 
-        const moorecoinsCell = document.createElement("td");
-        moorecoinsCell.innerHTML = `<form class="coin-edit">
+    const moorecoinsCell = document.createElement("td");
+    moorecoinsCell.innerHTML = `<form class="coin-edit">
                                         <input type="number" class="coin-edit-input" value="${user.moorecoins}">
                                         <button type="button" class="coin-edit-confirm">Confirm</button>
                                     </form>`;
-        row.appendChild(moorecoinsCell);
+    row.appendChild(moorecoinsCell);
 
-        const alertCell = document.createElement("td");
-        if (user.pending > 0) {
-            alertCell.innerHTML = ` <form class="coin-edit">
+    const alertCell = document.createElement("td");
+    if (user.pending > 0) {
+      alertCell.innerHTML = ` <form class="coin-edit">
                                         <button type="button" id="alert" class="coin-edit-confirm">Alert</button>
                                         <button type="button" id="copy" class="coin-edit-confirm">Copy</button>
                                         <button type="button" id="clear-${user.uid}" class="coin-edit-confirm">🧹</button>
                                     </form>`;
-        } else {
-            alertCell.textContent = "No Alerts";
-        }
-        row.appendChild(alertCell);
+    } else {
+      alertCell.textContent = "No Alerts";
+    }
+    row.appendChild(alertCell);
 
-        tableBody.appendChild(row);
+    tableBody.appendChild(row);
 
-        // Add event listener for the confirm button
-        const confirmButton = moorecoinsCell.querySelector(".coin-edit-confirm");
-        confirmButton.addEventListener("click", async () => {
-            const newMoorecoins = moorecoinsCell.querySelector(".coin-edit-input").value;
-            if (user && user.uid) {
-                await updateMoorecoins(user.uid, newMoorecoins);
-            } else {
-                console.error("User UID is undefined", user);
-            }
-        });
-
-        const hourConfirmButton = hourCell.querySelector(".coin-edit-confirm");
-        hourConfirmButton.addEventListener("click", async () => {
-            const newHour = hourCell.querySelector(".coin-edit-input").value;
-            if (user && user.uid) {
-                await updateHour(user.uid, newHour);
-            } else {
-                console.error("User UID is undefined", user);
-            }
-        });
-
-        const alertButton = alertCell.querySelector("#alert");
-        if (alertButton) {
-            alertButton.addEventListener("click", async () => {
-                if (user && user.uid) {
-                    alert(`${user.displayName} has submitted ${user.pending} MooreCoins.`);
-                } else {
-                    console.error("User UID is undefined", user);
-                }
-            });
-        }
-
-        const copyButton = alertCell.querySelector("#copy");
-        if (copyButton) {
-            copyButton.addEventListener("click", async () => {
-                if (user && user.uid) {
-                    navigator.clipboard.writeText(user.pending);
-                    alert(`Copied ${user.pending} MooreCoins to clipboard and cleared alerts.`);
-                    await updatePending(user.uid, 0);
-                } else {
-                    console.error("User UID is undefined", user);
-                }
-            });
-        }
-
-        const clearButton = document.getElementById(`clear-${user.uid}`);
-        if (clearButton) {
-            clearButton.addEventListener("click", async () => {
-                if (user && user.uid) {
-                    await updatePending(user.uid, 0);
-                } else {
-                    console.error("User UID is undefined", user);
-                }
-            });
-        }
+    // Add event listener for the confirm button
+    const confirmButton = moorecoinsCell.querySelector(".coin-edit-confirm");
+    confirmButton.addEventListener("click", async () => {
+      const newMoorecoins =
+        moorecoinsCell.querySelector(".coin-edit-input").value;
+      if (user && user.uid) {
+        await updateMoorecoins(user.uid, newMoorecoins);
+      } else {
+        console.error("User UID is undefined", user);
+      }
     });
+
+    const hourConfirmButton = hourCell.querySelector(".coin-edit-confirm");
+    hourConfirmButton.addEventListener("click", async () => {
+      const newHour = hourCell.querySelector(".coin-edit-input").value;
+      if (user && user.uid) {
+        await updateHour(user.uid, newHour);
+      } else {
+        console.error("User UID is undefined", user);
+      }
+    });
+
+    const alertButton = alertCell.querySelector("#alert");
+    if (alertButton) {
+      alertButton.addEventListener("click", async () => {
+        if (user && user.uid) {
+          alert(
+            `${user.displayName} has submitted ${user.pending} MooreCoins.`
+          );
+        } else {
+          console.error("User UID is undefined", user);
+        }
+      });
+    }
+
+    const copyButton = alertCell.querySelector("#copy");
+    if (copyButton) {
+      copyButton.addEventListener("click", async () => {
+        if (user && user.uid) {
+          navigator.clipboard.writeText(user.pending);
+          alert(
+            `Copied ${user.pending} MooreCoins to clipboard and cleared alerts.`
+          );
+          await updatePending(user.uid, 0);
+        } else {
+          console.error("User UID is undefined", user);
+        }
+      });
+    }
+
+    const clearButton = document.getElementById(`clear-${user.uid}`);
+    if (clearButton) {
+      clearButton.addEventListener("click", async () => {
+        if (user && user.uid) {
+          await updatePending(user.uid, 0);
+        } else {
+          console.error("User UID is undefined", user);
+        }
+      });
+    }
+  });
 }
 
 async function updateMoorecoins(userId, newMoorecoins) {
-    const db = getFirestore();
-    const userDoc = doc(db, "users", userId);
-    await updateDoc(userDoc, {
-        moorecoins: parseInt(newMoorecoins)
-    });
+  const db = getFirestore();
+  const userDoc = doc(db, "users", userId);
+  await updateDoc(userDoc, {
+    moorecoins: parseInt(newMoorecoins),
+  });
 }
 
 async function updateHour(userId, newHour) {
-    const db = getFirestore();
-    const userDoc = doc(db, "users", userId);
-    await updateDoc(userDoc, {
-        hour: parseInt(newHour)
-    });
+  const db = getFirestore();
+  const userDoc = doc(db, "users", userId);
+  await updateDoc(userDoc, {
+    hour: parseInt(newHour),
+  });
 }
 
 async function updatePending(userId, newPending) {
-    const db = getFirestore();
-    const userDoc = doc(db, "users", userId);
-    return updateDoc(userDoc, {
-        pending: parseInt(newPending)
-    });
+  const db = getFirestore();
+  const userDoc = doc(db, "users", userId);
+  return updateDoc(userDoc, {
+    pending: parseInt(newPending),
+  });
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
-    setupRealTimeListener();
+  setupRealTimeListener();
 });
