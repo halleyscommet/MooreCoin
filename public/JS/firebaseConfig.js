6c86b6c754fc68ed4c9bbf1d5784f0671abc84cc
Halley Harper <qwrty.streams@gmail.com>
2024-10-16 07:57:30 -0400

    update .gitignore

---
+++ public/JS/firebaseConfig.js
@@ -0,0 +1,9 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyBG58V6bm8YKtnKHmAytqY4msgvhb7KFGI",
+  authDomain: "moorecoin-65280.firebaseapp.com",
+  databaseURL: "https://moorecoin-65280-default-rtdb.firebaseio.com",
+  projectId: "moorecoin-65280",
+  storageBucket: "moorecoin-65280.appspot.com",
+  messagingSenderId: "20012522750",
+  appId: "1:20012522750:web:a776a35ff7a63cde3e59af",
+};
